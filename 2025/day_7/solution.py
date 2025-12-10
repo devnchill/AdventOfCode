d21840e0bb77ce9da8c7e53060738c5e1a31e48e
0xDev <169875752+devnchill@users.noreply.github.com>
2025-12-10 17:15:04 +0530

    feat: day 7 part 2

--- 2025/day_7/solution.py
+++ 2025/day_7/solution.py
@@ -19,7 +19,38 @@ def part1(data):
     return split(arr, a, b, 0, j)
 
 
+def split2(arr, a, b, i, j, cache):
+    while True:
+        if i < 0 or j < 0 or i >= a or j >= b:
+            return 0
+        if arr[i][j] == "X":
+            return 0
+        if (i, j) in cache:
+            return cache[(i, j)]
+        if arr[i][j] == "^":
+            break
+        i += 1
+    result = (
+        1
+        + split2(arr, a, b, i + 1, j - 1, cache)
+        + split2(arr, a, b, i + 1, j + 1, cache)
+    )
+
+    cache[(i, j)] = result
+    return result
+
+
+def part2(data):
+    arr = [list(a) for a in data.read().strip().split("\n")]
+    j = arr[0].index("S")
+    a = len(arr)
+    b = len(arr[0])
+    cache = {}
+    return split2(arr, a, b, 0, j, cache)
+
+
 with open("input.txt", encoding="utf-8") as file_object:
     print(part1(file_object))
     file_object.seek(0)
-    # print(part2(file_object))
+    print(part2(file_object) + 1)
+
